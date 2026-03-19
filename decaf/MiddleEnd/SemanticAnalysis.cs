84445fc3926ddc52186061ae55502d6afda55ffb
tonye4 <tpt.tran7@gmail.com>
2026-03-18 20:00:00 -0400

    feat(semantic): add method body traversal and string literal validation

--- decaf/MiddleEnd/SemanticAnalysis.cs
+++ decaf/MiddleEnd/SemanticAnalysis.cs
@@ -1,17 +1,19 @@
+using Decaf.IR.ParseTree;
 using System;
 using System.Linq;
+using System.Reflection;
 
-using Decaf.IR.ParseTree;
-using Decaf.Utils.Errors.SemanticErrors;
-
-namespace Decaf.MiddleEnd {
+namespace MiddleEnd {
+  // Possible exceptions that can be thrown during semantic analysis
+  // TODO: Add position information to these exceptions for better error reporting
+  public class SemanticException(string message) : Exception(message) { }
   // Performs semantic analysis on the parse tree, ensuring that the program is semantically valid
   public class SemanticChecker {
     private SemanticChecker() { }
     public static void CheckProgramNode(ProgramNode program) {
       // Ensure the program contains a class named "Program"
       if (!program.Classes.Any(m => m.Name == "Program")) {
-        throw new SemanticException(program.Position, "A program must contain a class called 'Program'");
+        throw new SemanticException("A program must contain a class called 'Program'");
       }
 
       foreach (var _class in program.Classes) {
@@ -25,33 +27,89 @@ namespace Decaf.MiddleEnd {
         // Find the Main method in the Program class
         var mainMethodCandidates = _class.Methods.Where(m => m.Name == mainName);
         if (mainMethodCandidates.Count() != 1) {
-          throw new SemanticException(_class.Position, "A main entry point must exist at `Program.Main()`");
+          throw new SemanticException("A main entry point must exist at `Program.Main()`");
         }
         // We get the first, note that the list is guaranteed to have exactly one element due to the previous check
         var mainMethod = mainMethodCandidates.First(m => m.Name == mainName);
         if (mainMethod.Parameters.Length != 0) {
-          throw new SemanticException(mainMethod.Position, "`Program.Main()` should not accept any parameters");
+          throw new SemanticException("`Program.Main()` should not accept any parameters");
         }
-        if (!(mainMethod.ReturnType.Type == PrimitiveType.Void)) {
-          throw new SemanticException(mainMethod.ReturnType.Position, "`Program.Main()` should not accept any parameters");
-        }
-      }
-      // TODO:- throw new SemanticException("A main entry point must exist in `Program.Main()`");
-      // TODO:- throw new SemanticException("`Program.Main()` must return void");
-    }
-  }
-}
-
-
-/*    if (_class.Name == "Program") {
-        if (!_class.MethodDeclarations.Any(m => m.Name == "Main")) {
-          throw new SemanticException("A main entry point must exist in `Program.Main()`");
-        }
-        var mainMethod = _class.MethodDeclarations.First(m => m.Name == "Main");
-        if (mainMethod.ReturnType != "void") {
+        if (!(mainMethod.ReturnType.Type is TypeNode.PrimitiveType.Void)) {
           throw new SemanticException("`Program.Main()` must return void");
         }
-        if (mainMethod.Parameters.Length != 0) {
-          throw new SemanticException("`Program.Main()` must accept zero parameters");
-        }
-      } */
+      }
+    }
+
+    private static void CheckMethodNode(DeclarationNode.MethodNode _methodDecl) {
+      // String literal can only occur in a CallNode if it IsPrimitive any other parent node is incorrect
+      // * The way to check this would be to pass the parentNode in a context struct and when you visit 
+      // a stringNode you bassically say hey if my parent isn't a callNode who has IsPrim then this is invalid.
+      var parentContext = ParentContext.Default();
+      CheckBlockNode(_methodDecl.Body, parentContext);
+    }
+
+    private static void CheckBlockNode(BlockNode block, ParentContext context) {
+      foreach (var statement in block.Statements) {
+        CheckStatementNode(statement, context);
+      }
+    }
+
+    private static void CheckStatementNode(StatementNode statement, ParentContext context) {
+      switch(statement) {
+        case StatementNode.AssignmentNode assign: 
+          CheckExpressionNode(assign.Expression, context);
+          break;
+        case StatementNode.ExprNode expr: 
+          CheckExpressionNode(expr.Content, context);
+          break;
+        case StatementNode.IfNode ifNode:
+          CheckExpressionNode(ifNode.Condition, context);
+          break;
+        case StatementNode.ReturnNode ret:
+          if (ret.Value != null)
+            CheckExpressionNode(ret.Value, context);
+          break;
+        case StatementNode.WhileNode whileNode:
+          CheckExpressionNode(whileNode.Condition, context);
+          CheckBlockNode(whileNode.Body, context);
+          break;
+      }
+    }
+
+    private static void CheckExpressionNode(ExpressionNode expression, ParentContext context) {
+    switch (expression) {
+        case ExpressionNode.CallNode call:
+            var innerCtx = new ParentContext { InPrimCall = call.IsPrimitive };
+            foreach (var arg in call.Arguments) {
+                CheckExpressionNode(arg, innerCtx);
+            }
+            break;
+        case ExpressionNode.BinopNode binop:
+            CheckExpressionNode(binop.Lhs, context);
+            CheckExpressionNode(binop.Rhs, context);
+            break;
+        case ExpressionNode.PrefixNode prefix:
+            CheckExpressionNode(prefix.Operand, context);
+            break;
+        case ExpressionNode.LocationNode location:
+            CheckExpressionNode(location.Root, context);
+            if (location.IndexExpr != null)
+                CheckExpressionNode(location.IndexExpr, context);
+            break;
+        case LiteralNode.StringNode:
+            if (!context.InPrimCall)
+                throw new SemanticException("String literals may only appear as arguments to a primitive call");
+            break;
+    }
+}
+
+    private struct ParentContext {
+      public bool InPrimCall { get; init; }
+
+      public static ParentContext Default() => new ParentContext {
+          InPrimCall = false
+      };
+    }
+
+    }
+  }
